35f8a4dc05755e1235b38443e3d9baa19b5d9ee2
Rodrigo Mariaca <rlmariac@cougarnet.uh.edu>
2022-01-14 14:18:32 -0600

    added validation and alert to contact page

--- contact.js
+++ contact.js
@@ -1,19 +1,54 @@
-(function () {
-    'use strict'
+function success(){
+  swal({
+      title: "Success!",
+      text: "Message sent successfully!",
+      icon: "success",
+      button: "close",
+  });
+}
+
+function errors(){
+  swal({
+      title: "Uh Oh!",
+      text: "There was a problem sending your message",
+      icon: "error",
+      button: "close",
+  });
+}
+
+function sendMail(form){
+  console.log("sendMail");
+  event.preventDefault()
+  form.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('cv_services', 'cv_services', form)
+          .then(function() {
+              success();
+              console.log('SUCCESS!');
+              
+          }, function(error) {
+              errors();
+              console.log('FAILED...', error);
+              
+          });
+}
+
+function validate(){
+  var form = document.querySelector('.needs-validation')
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+      form.addEventListener('submit', function (event) {
+      if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+          console.log("wrong");
+      }
+      else{
+          console.log("validated");
+          sendMail(form);     
+      }
+
+      form.classList.add('was-validated')
+      
+      }, false)
+}
+validate();
