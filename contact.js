d54abf54e45b6e35702891eb034470a21f319b32
Rodrigo Mariaca <rlmariac@cougarnet.uh.edu>
2022-01-15 15:34:12 -0600

    added reCaptcha and resume

--- contact.js
+++ contact.js
@@ -17,20 +17,27 @@ function errors(){
 }
 
 function sendMail(form){
-  console.log("sendMail");
-  event.preventDefault()
-  form.contact_number.value = Math.random() * 100000 | 0;
-      // these IDs from the previous steps
-      emailjs.sendForm('cv_services', 'cv_services', form)
-          .then(function() {
-              success();
-              console.log('SUCCESS!');
-              
-          }, function(error) {
-              errors();
-              console.log('FAILED...', error);
-              
-          });
+    console.log("sendMail");
+    event.preventDefault()
+    form.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+    if(grecaptcha.getResponse() == "") {
+        document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">This field is required.</span>';
+    }
+    else{
+        emailjs.sendForm('cv_services', 'cv_services', form)
+        .then(function() {
+            success();
+            console.log('SUCCESS!');
+            
+        }, function(error) {
+            errors();
+            console.log('FAILED...', error);
+            
+        });
+        grecaptcha.reset();
+    }
+    
 }
 
 function validate(){
@@ -50,5 +57,16 @@ function validate(){
       form.classList.add('was-validated')
       
       }, false)
+
+      
 }
+ 
+function verifyCaptcha(token) {
+    recaptcha_response = token;
+    document.getElementById('g-recaptcha-error').innerHTML = '';
+}
+
 validate();
+
+
+
